792fcd6735481ed98ecd820a45c993569940cf87
vbonnard <vbonnard.dev@gmail.com>
2026-01-29 19:29:57 +0100

    install wordpress but not config + domain name configured

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress est déjà installé"
+else
+
+	#Téléchargement de wordpress and de tous les fichiers de configurations
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* /var/www/html 
+	rm -rf latest.tar.gz
+	rm -rf wordpress
+
+fi 
+
+
 # On force la copie des fichiers de l'image vers le volume
 # On utilise un dossier temporaire /tmp/src comme source
-rm -rf /var/www/html/*
 cp -rf /tmp/src/* /var/www/html/
 
 # On s'assure que les permissions sont correctes
 chown -R www-data:www-data /var/www/html/
+
+
 
 # On lance la commande originale (PHP-FPM)
 exec "$@"
